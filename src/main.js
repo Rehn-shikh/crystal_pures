67a499a29d21c23d1ceaa63ad59b25e280ef99d1
niko <shakilrehan23@gmail.com>
2024-03-16 04:44:02 +0530

    modified:   index.html 	new file:   public/instagram.png 	new file:   public/whatsapp.png 	new file:   public/youtube.png 	modified:   src/main.js 	modified:   styles/components/header.css 	new file:   styles/components/hero.css 	new file:   styles/components/products.css 	modified:   styles/style.css 	modified:   styles/utils.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../styles/modern-normalise.css'
-import '../styles/components/header.css'
 import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/products.css'
 import '../styles/utils.css'
